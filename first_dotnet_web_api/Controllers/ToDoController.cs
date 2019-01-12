1c733d8be0cf18c26dca14da66f3555d21bed3d0
BPW <bwittenberg.developer@gmail.com>
2019-01-12 15:28:30 -0600

    Added basic api logic

---
+++ first_dotnet_web_api/Controllers/ToDoController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using first_dotnet_web_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace first_dotnet_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoController(ToDoContext context)
+        {
+            _context = context;
+
+            if (_context.ToDoItems.Count() == 0)
+            {
+                _context.ToDoItems.Add(new ToDoItem { Name = "Item 1" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetTodoItems()
+        {
+            return await _context.ToDoItems.ToListAsync();
+        }
+
+        // GET: api/Todo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.ToDoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+    }
+}
---
+++ first_dotnet_web_api/Models/ToDoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace first_dotnet_web_api.Models
+{
+    public class ToDoContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
